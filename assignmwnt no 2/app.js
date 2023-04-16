5c4dbebcd24d1ec381d5bb644e586bbce7414605
ZiaurRehmanArain <arainzia32@gmail.com>
2023-04-17 00:43:40 +0500

    Assignment no 2

--- assignmwnt no 2/app.js
+++ assignmwnt no 2/app.js
@@ -41,3 +41,75 @@ function addParagraph(text) {
 
   addParagraph("this my first experecce")
 
+// Question no 4
+
+function addListItem(text) {
+    // create a new list item element
+    var listItem = document.createElement("li");
+    
+    // set the text content of the list item
+    var listItemText = document.createTextNode(text);
+    listItem.appendChild(listItemText);
+    
+    // add the new list item to the unordered list with ID "myList"
+    var unorderedList = document.getElementById("myList");
+    unorderedList.appendChild(listItem);
+  }
+
+  addListItem('test')
+
+
+//   Question no 5
+
+function changeBackgroundColor(element, color) {
+    element.style.backgroundColor = color;
+  }
+
+  let div=document.getElementById('divs')
+changeBackgroundColor(div,'yellow')  
+  
+
+// Question no 6
+
+function saveToLocalStorage(key, object) {
+    localStorage.setItem(key, JSON.stringify(object));
+  }
+  const myObject = { name: "zia", age: 20 };
+  saveToLocalStorage("myObject", myObject);
+
+
+//   Question no 7
+
+function getObjectFromLocalStorage(key) {
+    const item = localStorage.getItem(key);
+    return JSON.parse(item);
+  }
+
+//   Question no 8
+
+function saveToLocalStorage(obj) {
+    for (const prop in obj) {
+      localStorage.setItem(prop, obj[prop]);
+    }
+    
+    const newObj = {};
+    for (const prop in obj) {
+      newObj[prop] = localStorage.getItem(prop);
+    }
+    
+    return newObj;
+  }
+
+
+  const myObj = { 
+    name: 'zia', 
+    age: 20, 
+    city: 'Hyderabad' 
+  };
+  
+  const savedObj = saveToLocalStorage(myObj);
+  
+  console.log(savedObj); 
+  
+  
+    
